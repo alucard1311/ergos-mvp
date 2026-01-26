c02c821a4e978f98b6ba93408f1072a47f4fc982
Vinay <vinay.naidu1311@gmail.com>
2026-01-26 16:09:05 -0500

    feat(08-02): update transport exports
    - Add ConnectionManager export
    - Add create_signaling_app export
    - Update __all__ list with new public APIs

--- src/ergos/transport/__init__.py
+++ src/ergos/transport/__init__.py
@@ -1,7 +1,9 @@
 """WebRTC transport module for Ergos."""
 
 from ergos.transport.audio_track import AUDIO_PTIME, TTSAudioTrack
+from ergos.transport.connection import ConnectionManager
 from ergos.transport.data_channel import DataChannelHandler
+from ergos.transport.signaling import create_signaling_app
 from ergos.transport.types import (
     DataChannelMessage,
     SignalingRequest,
@@ -20,6 +22,10 @@ __all__ = [
     # Audio track
     "TTSAudioTrack",
     "AUDIO_PTIME",
+    # Connection management
+    "ConnectionManager",
+    # Signaling
+    "create_signaling_app",
     # Data channel
     "DataChannelHandler",
 ]
